bd2be9453cbfbc33b36f98b2a531678713504c99
Slava Yanuskevich <cool.9nyw@gmail.com>
2023-04-04 20:23:12 +0300

    add visual walk

---
+++ rw_visual.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+
+# Построение случайного блуждания.
+rw = RandomWalk()
+rw.fill_walk()
+
+# Нанесение точек на диаграмму.
+plt.style.use('classic')
+fig, ax = plt.subplots()
+ax.scatter(rw.x_values, rw.y_values, c='red', s=15)
+plt.show()
