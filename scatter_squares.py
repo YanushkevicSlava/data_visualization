618828342dec9914f7d2bb256a58818b4dddee24
Slava Yanuskevich <cool.9nyw@gmail.com>
2023-04-04 19:50:00 +0300

    add scatter and color

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
-plt.style.use('seaborn-v0_8')
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds,  s=10)
 
 # Назначение заголовка диаграммы и меток осей.
 ax.set_title("Square Numbers", fontsize=24)
@@ -14,4 +14,8 @@ ax.set_ylabel("Square of Value", fontsize=14)
 # Назначение размера шрифта делений на осях.
 ax.tick_params(axis='both', which='major', labelsize=14)
 
+# Назначение диапазона для каждой оси.
+ax.axis([0, 1100, 0, 1100000])
+
+# plt.savefig('squares_plot.png', bbox_inches='tight')
 plt.show()
