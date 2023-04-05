9ece55becf469aaabdb82974185bd412557a0235
Slava Yanuskevich <cool.9nyw@gmail.com>
2023-04-05 05:41:13 +0300

    add ren more walk

--- rw_visual.py
+++ rw_visual.py
@@ -1,12 +1,19 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-# Построение случайного блуждания.
-rw = RandomWalk()
-rw.fill_walk()
+# Новые блужданя строятся до тех пор, пока программа остаётся активной.
+while True:
+    # Построение случайного блуждания.
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Нанесение точек на диаграмму.
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, c='red', s=15)
-plt.show()
+    # Нанесение точек на диаграмму.
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, c='red', s=15)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ").lower()
+    if keep_running == 'n':
+        break
+
