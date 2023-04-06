291a29e39f7ba3177f44d9a607d5710fea4f6bb3
Slava Yanuskevich <cool.9nyw@gmail.com>
2023-04-06 11:54:51 +0300

    add more points

--- rw_visual.py
+++ rw_visual.py
@@ -4,14 +4,14 @@ from random_walk import RandomWalk
 # Новые блужданя строятся до тех пор, пока программа остаётся активной.
 while True:
     # Построение случайного блуждания.
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     # Нанесение точек на диаграмму.
     plt.style.use('classic')
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Reds, edgecolors='none', s=15)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Reds, edgecolors='none', s=1)
 
     # Выделение первой и последней точек.
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
