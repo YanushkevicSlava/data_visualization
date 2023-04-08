1aeb26e9b3e00b31359932dc55ac08f2a358a9ca
Slava Yanuskevich <cool.9nyw@gmail.com>
2023-04-08 05:13:59 +0300

    add a D10 cube

--- die_visual.py
+++ die_visual.py
@@ -2,13 +2,13 @@ from plotly.graph_objs import Bar, Layout
 from plotly import offline
 from die import Die
 
-# Создание двух кубиков D6.
+# Создание кубиков D6 и D10.
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 # Моделирование серии бросков с сохранением результатов в списке.
 results = []
-for roll_num in range(1000):
+for roll_num in range(50000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -25,6 +25,6 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Result of rolling two D6 1000 times',
+my_layout = Layout(title='Result of rolling a D6 and a D10 50000 times',
                    xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
