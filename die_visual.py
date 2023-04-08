d90d946e0c3df845112059316bfa9407de31e758
Slava Yanuskevich <cool.9nyw@gmail.com>
2023-04-08 04:56:08 +0300

    add diagramming

--- die_visual.py
+++ die_visual.py
@@ -1,3 +1,5 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
 from die import Die
 
 # Создание кубика.
@@ -15,4 +17,12 @@ for value in range(1, die.num_sides + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
-print(frequencies)
+# Визуализация результатов.
+x_values = list(range(1, die.num_sides + 1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(title='Result of rolling one D6 1000 times',
+                   xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
