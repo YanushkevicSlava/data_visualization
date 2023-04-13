13f64be0b3a6f72e8fc03761410a5d78d71e0fa4
Slava Yanuskevich <cool.9nyw@gmail.com>
2023-04-13 20:01:43 +0300

    add temperature chart

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,4 +1,6 @@
 import csv
+from matplotlib import pyplot as plt
+from datetime import datetime
 
 
 filename = 'data/sitka_weather_07-2021_simple.csv'
@@ -9,10 +11,21 @@ with open(filename) as f:
 # for index, column_header in enumerate(header_row):
 #     print(index, column_header)
 
-    # Чтение максимальных температур.
-    highs = []
+    # Чтение максимальных температур и дат из файла.
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
         high = int(row[5])
+        dates.append(current_date)
         highs.append(high)
 
-print(highs)
+# Нанесение данных на диаграмму.
+plt.style.use('classic')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red')
+
+# Форматирование диаграммы.
+plt.title("Daily high trmperatures, July 2021", fontsize=24)
+plt.xlabel('', fontsize=16)
+
+plt.show()
