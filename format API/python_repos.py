3c234e267325587dc9f5b63912bf7c193b29e799
Slava Yanuskevich <cool.9nyw@gmail.com>
2023-04-19 19:34:28 +0300

    information analysis in 1 repos

--- format API/python_repos.py
+++ format API/python_repos.py
@@ -1,13 +1,24 @@
 import requests
 
 # Создание вызова API и сохранение ответа.
-url = 'htts://api,github.com/search/repositiries?q=language:python&sort=stars'
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 print(f'Statuse code: {r.status_code}')
 
 # Сохранение ответа API в переменной.
 response_dict = r.json()
+print(f'Total repositories: {response_dict["total_count"]}')
+
+# Анализ информации о репозиториях.
+repo_dicts = response_dict['items']
+print(f'Repositories returned: {len(repo_dicts)}')
+
+# Анализ первого репозитория.
+repo_dict = repo_dicts[0]
+print(f'\nKeys: {len(repo_dict)}')
+for key in sorted(repo_dict.keys()):
+    print(key)
 
 # Обработка результатов.
 print(response_dict.keys())
