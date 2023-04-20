5cc4c542544494d7e4e3d0de00080b599af18317
Slava Yanuskevich <cool.9nyw@gmail.com>
2023-04-20 05:51:10 +0300

    add links

--- format API/python_repos_visual.py
+++ format API/python_repos_visual.py
@@ -12,9 +12,12 @@ print(f'Statuse code: {r.status_code}')
 # Обработка результатов.
 response_dict = r.json()
 repo_dicts = response_dict['items']
-pero_names, stars, labels = [], [], []
+repo_links, stars, labels = [], [], []
 for repo_dict in repo_dicts:
-    pero_names.append(repo_dict['name'])
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f'<a href="{repo_url}">{repo_name}</a>'
+    repo_links.append(repo_link)
     stars.append(repo_dict['stargazers_count'])
 
     owner = repo_dict['owner']['login']
@@ -25,7 +28,7 @@ for repo_dict in repo_dicts:
 # Построение визуализации.
 data = [{
     'type': 'bar',
-    'x': pero_names,
+    'x': repo_links,
     'y': stars,
     'hovertext': labels,
     'marker': {
